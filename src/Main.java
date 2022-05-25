5621e795f80b45ddbb0cbb0917e27a673c11c75c
stokjko98 <104865498+stokjko98@users.noreply.github.com>
2022-05-25 19:30:17 +0200

    Add Stojkovic Class to Main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         BEDA_TestClass beda_testClass = new BEDA_TestClass();
         System.out.println(beda_testClass.toString());
+        STOJKOVIC_TestClass stojkovic_testClass = new STOJKOVIC_TestClass();
+        System.out.println(stojkovic_testClass.toString());
     }
 }
